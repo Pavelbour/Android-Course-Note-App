e74eace16aa5554258c616588c558c3d345722b9
Pavelbour <pavelbour@gmail.com>
2022-02-02 22:27:05 +0300

    Launch stats enabled.

--- app/src/main/java/ru/gb/androidcoursenoteapp/ui/Controller.java
+++ app/src/main/java/ru/gb/androidcoursenoteapp/ui/Controller.java
@@ -5,6 +5,7 @@ import ru.gb.androidcoursenoteapp.domain.NoteEntity;
 public interface Controller {
     void showNoteFragment(NoteEntity noteEntity);
     void showNewNoteFragment();
+    void showAppStats();
     void onAddNote();
     void onEditNote(NoteEntity noteEntity);
     void onDeleteNote(NoteEntity noteEntity);
